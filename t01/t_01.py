0a1c883fffbd9368a65b278565d591b9f6ced7f5
Blacksolid64 <48729275+Blacksolid64@users.noreply.github.com>
2022-03-21 17:18:56 -0600

    Actualización R1,R3
    Se añaden las funciones R1 y R3 al repositorio

--- t01/t_01.py
+++ t01/t_01.py
@@ -43,7 +43,12 @@ def R0(fecha_es_tupla):
 #R1 ---------------------------------------------------------------------
 #joshua
 def R1(fecha_es_valida):
-    return (bool(1))
+    if (fecha_es_valida%4 == 0):
+		return True
+	elif (fecha_es_valida%100 == 0) and (fecha_es_valida%400 == 0):
+		return True
+	else:
+		return False
 
 
 #R2 --------------------     fecha valida G.     ------------------------------
@@ -78,7 +83,36 @@ def R2(fecha_es_valida):
 
 #R3 --------------------     dia siguiente       -------------------------------------
 #joshua
-
+def R3(dia_siguiente): #Calcula el dia siguiente, dado una fecha. Puede mejorarse
+	Dias_31 = [1,3,5,7,8,10,12]
+	Dias_30 = [4,6,9,11]
+	if (R2(dia_siguiente) == True):
+		esBisiesto = bisiesto(dia_siguiente[0])
+		dia = dia_siguiente[2]
+		mes = dia_siguiente[1]
+		if (mes in Dias_31) and (dia == 31):
+			fecha = (dia_siguiente[0],dia_siguiente[1]+1,1)
+			return dia_siguiente
+		if (mes in Dias_30) and (dia == 30):
+			fecha = (dia_siguiente[0],dia_siguiente[1]+1,1)
+			return dia_siguiente
+		if (esBisiesto) and (mes == 2):
+			if (dia == 29):
+				dia_siguiente = (dia_siguiente[0],dia_siguiente[1]+1,1)
+				return dia_siguiente
+			else:
+				dia_siguiente = (dia_siguiente[0],dia_siguiente[1],dia_siguiente[2]+1)
+				return dia_siguiente
+		elif (mes == 2) and (dia == 28):
+			fecha = (dia_siguiente[0],dia_siguiente[1]+1,1)
+			return dia_siguiente
+		else:
+			dia_siguiente = (dia_siguiente[0],dia_siguiente[1],dia_siguiente[2]+1)
+			return dia_siguiente
+		#01,03,05,07,08,10,12
+		#04,06,09,11
+	else:
+		print("La fecha ingresada no es válida")
             
 #R4 --------------------     ordinal             ------------------------------------------
 #mari
