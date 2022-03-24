736b576c2774765758afa06a4927ce675fd9a0fe
mari <>
2022-03-24 16:20:20 -0600

    Update t_01.py

--- t01/t_01.py
+++ t01/t_01.py
@@ -1,83 +1,86 @@
 #---------------------------Referencias------------------------------------------------------
 #https://pyformat.info/#string_pad_align
 
-# ---------------------  Funciones  Axuiliares------------------------------------------------
-def cal_G(dia, mes, anno):
-    if(mes==2 and dia<=28):
-        return (bool(1))
-    
-    elif (R2(anno) and dia<=29):
-        return (bool(1))
-    
-    elif(mes==4 or mes==6 or mes==9 or mes==11 and dia<=30):
-        return (bool(1))
 
-    elif(mes==1 or mes==3 or mes==5 or mes==7 or mes==8 or mes==10 or mes==12 and dia<=31):
-        return (bool(1))
-    
-    else:
-        print('Cantidad de dias incorrecto')
-        return (bool(0))
 #R0 ---------------------------------------------------------------------
 #mari
 def R0(fecha_es_tupla):
     if(len(fecha_es_tupla) != 3):
-        print('Fecha no valida')
+        print('tupla no valida')
         return (bool(0))
     else:
         for i in range(3):
             if (fecha_es_tupla[i]<= 0):
-                print('negativo')
+                print('no todos son enteros positivos')
                 return (bool(0))
-        if (len(str(fecha_es_tupla[0]))==4):
-            if (fecha_es_tupla[1]<=12):
-                if (fecha_es_tupla[2]<=32):
-                    print('Fecha valida')
-                    return (bool(1))
-            else:
-                 print('Mes no valida')
-                 return (bool(0))
-        else:            
-             print('Año no valida')
+        if (fecha_es_tupla[1]<=12):
+            if (fecha_es_tupla[2]<=32):
+                print('tupla valida')
+                return (bool(1))
+        else:
+             print('Mes no valida')
              return (bool(0))
 
+#(15,1,12) = valido     (2000,2,0)=falso
 
 
 #R1 ---------------------------------------------------------------------
 #joshua
 def R1(fecha_es_valida):
-	if (fecha_es_valida%4 == 0):
-		return True
-	elif (fecha_es_valida%100 == 0) and (fecha_es_valida%400 == 0):
-		return True
-	else:
-		return False
+    if(fecha_es_valida >= 1582):
+        if(fecha_es_valida%4 == 0):
+            return True
+        elif (fecha_es_valida%100 == 0) and (fecha_es_valida%400 == 0):
+            return True
+        else:
+            return False
 
 #R2 --------------------     fecha valida G.     ------------------------------
 #mari
 
 def cal_G(dia, mes, anno):
-    if(mes==2 and dia<=28):
-        return (bool(1))
-    
-    elif (R1(anno) and dia<=29):
-        return (bool(1))
-    
-    elif(mes==4 or mes==6 or mes==9 or mes==11 and dia<=30):
-        return (bool(1))
+    Dias_31 = [1,3,5,7,8,10,12]
+    Dias_30 = [4,6,9,11]
 
-    elif(mes==1 or mes==3 or mes==5 or mes==7 or mes==8 or mes==10 or mes==12 and dia<=31):
+    if(anno==1582):
+        if (mes==10 and dia>=15 and dia<=31):
+            return (bool(1))
+        elif (mes==11 and dia<=30):
+            return (bool(1))
+        elif (mes==12 and dia<=31):
+            return (bool(1))
+        else:
+            return (bool(0))
+        
+    elif(mes==2 and dia<=28):
         return (bool(1))
-    
+        
+    elif (R1(anno)and dia<=29):
+        return (bool(1))
+        
+    elif(mes in Dias_30 and dia<=30):
+        print(dia)
+        return (bool(1))
+        
+    elif(mes in Dias_31 and dia<=31):
+        return (bool(1))
+        
     else:
         print('Cantidad de dias incorrecto')
         return (bool(0))
     
 def R2(fecha_es_valida):
     if(R0(fecha_es_valida)):
-        if (cal_G(fecha_es_valida[2],fecha_es_valida[1],fecha_es_valida[0])):
-            print('Fecha valida calendario gregoriano')
-            return (bool(1))
+        if(fecha_es_valida[0]>=1582):
+            if (cal_G(fecha_es_valida[2],fecha_es_valida[1],fecha_es_valida[0])):
+                print('Fecha valida calendario gregoriano')
+                return (bool(1))
+            else:
+                print('Fecha invalida calendario gregoriano')
+                return (bool(0))
+        else:
+            print('año fuera de rango')
+            return (bool(0))
     else:
         print('Fecha no valida')
         return (bool(0))
@@ -118,6 +121,18 @@ def R3(dia_siguiente): #Calcula el dia siguiente, dado una fecha. Puede mejorars
             
 #R4 --------------------     ordinal             ------------------------------------------
 #mari
+def R4(ordinal_dia):
+    Dias_31 = [1,3,5,7,8,10,12]
+    Dias_30 = [4,6,9,11]
+    dias = 0
+    if(R2(ordinal_dia)):
+        for i in range(ordinal_dia[1]):
+            if (i in Dias_30):
+                dias = dias + 31
+            elif (i in Dias_31):
+                dias = dias + 30
+        dias = dias + ordinal_dia[2]
+        return (dias)
             
 #R5 --------------------     calendario          ------------------------------------------
 #joshua
